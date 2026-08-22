c1ee2490ab73633f61403e4e91c7acfd3e718479
xasan <muhammadovich.09@gmail.com>
2026-08-22 15:46:42 +0500

    admin panel added

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, UserConfirmation
 
-# Register your models here.
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'phone_number')
+
+admin.site.register(User, UserModelAdmin)
+
+class UserConfirmationModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'user__phone_number', "expiration_time", "is_confirmed")
+admin.site.register(UserConfirmation, UserConfirmationModelAdmin)
