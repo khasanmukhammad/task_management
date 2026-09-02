082b4c50440804248c4c47ee25d93b0660cf7623
xasan <muhammadovich.09@gmail.com>
2026-09-02 17:30:41 +0500

    tasks serializer added

---
+++ tasks/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from tasks.models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    title = serializers.CharField()
+    description = serializers.CharField()
+    task_status = serializers.CharField()
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
+
+    class Meta:
+        model = Task
+        fields = ["id", "title", "description", "task_status",
+            "created_at", "updated_at"]
+
+class TaskCreateSerializer(serializers.ModelSerializer):
+    title = serializers.CharField()
+    description = serializers.CharField()
+
+    class Meta:
+        model = Task
+        fields = ["title", "description"]
